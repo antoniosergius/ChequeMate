0121c6067a44f6e9900d1d787d5708a9b9eb4fea
Antonio <antonio.sergius@gmail.com>
2017-08-10 15:09:15 -0300

    Alterações no ViewSignature.

--- src/br/com/antoniosergius/chequemate/gui/dialog/ViewSignature.java
+++ src/br/com/antoniosergius/chequemate/gui/dialog/ViewSignature.java
@@ -11,15 +11,44 @@ public class ViewSignature extends javax.swing.JDialog {
    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
    private void initComponents() {
 
-      jXHeader = new org.jdesktop.swingx.JXHeader();
+      panelMain = new org.jdesktop.swingx.JXPanel();
+      header = new org.jdesktop.swingx.JXHeader();
+      toolBar = new javax.swing.JToolBar();
+      buttonImprimir = new org.jdesktop.swingx.JXButton();
+      jXButton2 = new org.jdesktop.swingx.JXButton();
+      jXButton3 = new org.jdesktop.swingx.JXButton();
+      jSeparator1 = new javax.swing.JToolBar.Separator();
+      jXButton4 = new org.jdesktop.swingx.JXButton();
       jXImageView1 = new org.jdesktop.swingx.JXImageView();
-      jToolBar1 = new javax.swing.JToolBar();
-      jButton1 = new javax.swing.JButton();
-      jButton2 = new javax.swing.JButton();
 
       setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-      jXHeader.setToolTipText("");
+      toolBar.setRollover(true);
+
+      buttonImprimir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/x22.print.png"))); // NOI18N
+      buttonImprimir.setFocusable(false);
+      buttonImprimir.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+      buttonImprimir.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+      toolBar.add(buttonImprimir);
+
+      jXButton2.setText("jXButton2");
+      jXButton2.setFocusable(false);
+      jXButton2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+      jXButton2.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+      toolBar.add(jXButton2);
+
+      jXButton3.setText("jXButton3");
+      jXButton3.setFocusable(false);
+      jXButton3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+      jXButton3.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+      toolBar.add(jXButton3);
+      toolBar.add(jSeparator1);
+
+      jXButton4.setText("jXButton4");
+      jXButton4.setFocusable(false);
+      jXButton4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+      jXButton4.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+      toolBar.add(jXButton4);
 
       javax.swing.GroupLayout jXImageView1Layout = new javax.swing.GroupLayout(jXImageView1);
       jXImageView1.setLayout(jXImageView1Layout);
@@ -29,42 +58,37 @@ public class ViewSignature extends javax.swing.JDialog {
       );
       jXImageView1Layout.setVerticalGroup(
          jXImageView1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addGap(0, 288, Short.MAX_VALUE)
+         .addGap(0, 319, Short.MAX_VALUE)
       );
 
-      jToolBar1.setRollover(true);
-
-      jButton1.setText("jButton1");
-
-      jButton2.setText("jButton2");
+      javax.swing.GroupLayout panelMainLayout = new javax.swing.GroupLayout(panelMain);
+      panelMain.setLayout(panelMainLayout);
+      panelMainLayout.setHorizontalGroup(
+         panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addComponent(header, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+         .addComponent(toolBar, javax.swing.GroupLayout.DEFAULT_SIZE, 782, Short.MAX_VALUE)
+         .addComponent(jXImageView1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+      );
+      panelMainLayout.setVerticalGroup(
+         panelMainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addGroup(panelMainLayout.createSequentialGroup()
+            .addComponent(header, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(toolBar, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(jXImageView1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(0, 40, Short.MAX_VALUE))
+      );
 
       javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
       getContentPane().setLayout(layout);
       layout.setHorizontalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addComponent(jXHeader, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-         .addComponent(jXImageView1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-         .addComponent(jToolBar1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-            .addContainerGap(620, Short.MAX_VALUE)
-            .addComponent(jButton2)
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(jButton1)
-            .addContainerGap())
+         .addComponent(panelMain, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
       );
       layout.setVerticalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addGroup(layout.createSequentialGroup()
-            .addComponent(jXHeader, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(jXImageView1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-               .addComponent(jButton1)
-               .addComponent(jButton2))
-            .addContainerGap())
+         .addComponent(panelMain, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
       );
 
       pack();
@@ -110,10 +134,14 @@ public class ViewSignature extends javax.swing.JDialog {
    }
 
    // Variables declaration - do not modify//GEN-BEGIN:variables
-   private javax.swing.JButton jButton1;
-   private javax.swing.JButton jButton2;
-   private javax.swing.JToolBar jToolBar1;
-   private org.jdesktop.swingx.JXHeader jXHeader;
+   private org.jdesktop.swingx.JXButton buttonImprimir;
+   private org.jdesktop.swingx.JXHeader header;
+   private javax.swing.JToolBar.Separator jSeparator1;
+   private org.jdesktop.swingx.JXButton jXButton2;
+   private org.jdesktop.swingx.JXButton jXButton3;
+   private org.jdesktop.swingx.JXButton jXButton4;
    private org.jdesktop.swingx.JXImageView jXImageView1;
+   private org.jdesktop.swingx.JXPanel panelMain;
+   private javax.swing.JToolBar toolBar;
    // End of variables declaration//GEN-END:variables
 }
